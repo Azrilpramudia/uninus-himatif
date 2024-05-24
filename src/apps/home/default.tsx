769b17d941b8088ea65e626ab0fb3bb04173a735
Azrilpramudia <azrilpramudia01@gmail.com>
2024-05-24 20:35:35 +0700

    feat: about-section

--- src/apps/home/default.tsx
+++ src/apps/home/default.tsx
@@ -1,12 +1,14 @@
 import { Navbar } from "@/components";
 import { Hero } from "./modules";
 import { FC, ReactElement } from "react";
+import { About } from "./modules/about";
 
 export const LandingPage: FC = (): ReactElement => {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   );
 };

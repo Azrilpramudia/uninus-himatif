f6a61fb6fd943ee93ac0a461438f4c196311e302
Azrilpramudia <azrilpramudia01@gmail.com>
2024-05-28 23:03:27 +0700

    feat: sellingpoint-section

---
+++ src/routes/home/modules/sellingpoint.tsx
@@ -0,0 +1,35 @@
+import { FC, ReactElement } from "react";
+
+export const SellingPoint: FC = (): ReactElement => {
+  return (
+    <section className="p-8">
+      <h2 className="px-9 text-4xl font-bold mb-4 text-blue-900">Program Kerja</h2>
+      <p className="px-9 mb-8">Berikut Program Kerja Periode Kepengurusan 2024 - 2025</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-9">
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold mb-2 text-blue-900">Melakukan penilaian diri</h3>
+          <p>
+            Pahami kondisi mental Anda dengan menyelesaikan tes dan kuestioner yang mudah digunakan.
+          </p>
+        </div>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold mb-2 text-blue-900">Mendapatkan Analisis AI</h3>
+          <p>
+            Dapatkan saran yang dipersonalisasi berdasarkan data diri dan keluhan Anda, dianalisis
+            dengan teknologi AI canggih.
+          </p>
+        </div>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold mb-2 text-blue-900">Temukan Bantuan Professional</h3>
+          <p>
+            Temukan faskes dan psikolog terdekat yang sesuai dengan kebutuhan Anda dengan mudah.
+          </p>
+        </div>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold mb-2 text-blue-900">Pelajari Lebih Dalam</h3>
+          <p>Akses informasi dan edukasi terpercaya tentang berbagai topik kesehatan mental.</p>
+        </div>
+      </div>
+    </section>
+  );
+};

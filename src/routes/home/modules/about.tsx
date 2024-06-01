e09a80da8191ebef12903e3932c030831a048322
Azrilpramudia <azrilpramudia01@gmail.com>
2024-06-01 13:18:56 +0700

    feat: fix color

--- src/routes/home/modules/about.tsx
+++ src/routes/home/modules/about.tsx
@@ -4,7 +4,7 @@ export const About: FC = (): ReactElement => {
   return (
     <section className="py-20 flex w-fullflex w-full min-h-2 items-center justify-center gap-x-30 px-12">
       <div className="container mx-auto px-4">
-        <h2 className="font-bold text-4xl mb-4 text-blue-900">TENTANG KAMI</h2>
+        <h2 className="font-bold text-4xl mb-4 text-[#020181]">TENTANG KAMI</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         <p>Unde mollitia animi quod amet, nemo laborum ea totam minima incidunt,</p>
         <p>accusamus voluptates dolor facere veritatis voluptate architecto hic.</p>
--- src/routes/home/modules/hero.tsx
+++ src/routes/home/modules/hero.tsx
@@ -2,12 +2,12 @@ import { FC, ReactElement } from "react";
 
 export const Hero: FC = (): ReactElement => {
   return (
-    <section className="flex w-full min-h-2 items-center justify-center gap-x-56 px-16 bg-gray-200">
+    <section className="flex w-full min-h-2 items-center justify-center gap-x-56 px-16 bg-[#F1F3FF]">
       <div className="w-1/2">
         <h2 className="font-bold text-4xl mb-3">GLORY HIMATIF</h2>
         <p>Welcome to Himpunan Mahasiswa Teknik Informatika</p>
         <p className="mb-6">Universitas Islam Nusantara</p>
-        <button className="bg-blue-900 py-1 px-3 text-center text-white font-semibold rounded">
+        <button className="bg-[#020181] py-1 px-3 text-center text-white font-semibold rounded">
           Masuk
         </button>
       </div>
